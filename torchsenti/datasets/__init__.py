98ac42a478f39de46fa65763cc0702e1b46cca39
Andreas Chandra <phoenix.ichigo@yahoo.com>
2020-08-04 10:14:28 +0000

    add dataset in init

--- torchsenti/datasets/__init__.py
+++ torchsenti/datasets/__init__.py
@@ -1,10 +1,15 @@
-
 from torchsenti.datasets.imdb import IMDB
 from torchsenti.datasets.trip_advisor import TripAdvisor
 from torchsenti.datasets.movie_review import MovieReview
+from torchsenti.datasets.congressional_speech import CongressSpeech
+from torchsenti.datasets.customer_review import CustomerReview
+from torchsenti.datasets.nine_product_review import NineProductReview
 
 __all__ = [
     'IMBD',
     'TripAdvisor',
-    'MovieReview'
+    'MovieReview',
+    'CongressSpeech',
+    'CustomerReview',
+    'NineProductReview'
 ]
